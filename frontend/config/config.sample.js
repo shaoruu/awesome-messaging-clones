28d3e94a50854fcd8c3243c987a1ff2bc78cdaa2
dvfcfbgv <21ianh1@tas.tw>
2019-02-13 21:25:58 +0800

    initApollo done

---
+++ frontend/config/config.sample.js
@@ -0,0 +1,5 @@
+/**
+ * GraphQL configuration
+ */
+export const GQL_BACKEND_URI = 'http://localhost:8000/graphql/'
+export const GQL_WS_URI = 'ws://localhost:8000/subscriptions'
---
+++ frontend/lib/graphql/initApollo.js
@@ -0,0 +1,73 @@
+import fetch from 'isomorphic-unfetch'
+import { createHttpLink } from 'apollo-link-http'
+import { GQL_BACKEND_URI, GQL_WS_URI } from '../../config/config'
+import { WebSocketLink } from 'apollo-link-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+import { setContext } from 'apollo-link-context'
+import { ApolloClient, InMemoryCache } from 'apollo-boost'
+
+let apolloClient = null
+
+// Polyfill fetch() on server (used by apollo-client)
+if (!process.browser) {
+	global.fetch = fetch
+}
+
+function create(initialState, { getToken }) {
+	const httpLink = createHttpLink({
+		uri: GQL_BACKEND_URI,
+		credentials: 'same-origin'
+	})
+
+	// Check out https://github.com/apollographql/subscriptions-transport-ws/issues/333 to see why I used process.browser
+	const wsLink = process.browser
+		? new WebSocketLink({
+				uri: GQL_WS_URI,
+				options: {
+					reconnect: true
+				}
+		  })
+		: null
+
+	const link = process.browser
+		? split(
+				({ query }) => {
+					const { kind, operation } = getMainDefinition(query)
+					return kind === 'OperationDefinition' && operation === 'subscription'
+				},
+				wsLink,
+				httpLink
+		  )
+		: httpLink
+
+	const authLink = setContext((_, { headers }) => {
+		const token = getToken()
+		return {
+			headers: {
+				...headers,
+				authorization: token ? `JWT ${token}` : ''
+			}
+		}
+	})
+
+	return new ApolloClient({
+		link: authLink.concat(link),
+		cache: new InMemoryCache().restore(initialState || {})
+	})
+}
+
+export default function initApollo(initialState, options) {
+	// Make sure to create a new client for every server-side request so that data
+	// isn't shared between connections (which would be bad)
+	if (!process.browser) {
+		return create(initialState, options)
+	}
+
+	// Reuse client on the client-side
+	if (!apolloClient) {
+		apolloClient = create(initialState, options)
+	}
+
+	return apolloClient
+}
